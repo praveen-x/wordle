3a197fc7d7053ec1783ff5a88d0a3b59e06b7009
praveen-x <12800670+praveen-x@users.noreply.github.com>
2022-11-20 17:56:52 +0530

    basic setup

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+const GAMES: &str = include_str!("../answers.txt");
+fn main() {    
+    for answer in GAMES.split_whitespace() {
+        let guesser = wordle::algorithms::Naive::new();
+        wordle::play(answer, guesser);
+    }
+}
